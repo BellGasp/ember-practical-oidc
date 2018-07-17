e13e8098b641ea9e54019729814d63054f1c7224
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2018-07-17 11:20:59 -0700

    adds ember-cli-cjs blueprint

--- blueprints/oidc-client/index.js
+++ blueprints/oidc-client/index.js
@@ -5,8 +5,16 @@ module.exports = {
   },
   afterInstall() {
     // Add addons to package.json and run defaultBlueprint
-    return this.addPackagesToProject([
-      { name: 'oidc-client', target: '1.5.2' }
-    ]);
+    return this.addAddonsToProject({
+      // a packages array defines the addons to install
+      packages: [
+        // name is the addon name, and target (optional) is the version
+        { name: 'ember-cli-cjs-transform', target: '1.3.0' }
+      ]
+    }).then(() => {
+      return this.addPackagesToProject([
+        { name: 'oidc-client', target: '1.5.2' }
+      ]);
+    });
   }
 };
