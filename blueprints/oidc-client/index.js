4d9f606aa217d8005cb5a291d7874387bcb46fcb
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2018-07-17 11:10:34 -0700

    removes addPackages

--- blueprints/oidc-client/index.js
+++ blueprints/oidc-client/index.js
@@ -5,16 +5,8 @@ module.exports = {
   },
   afterInstall() {
     // Add addons to package.json and run defaultBlueprint
-    return this.addAddonsToProject({
-      // a packages array defines the addons to install
-      packages: [
-        // name is the addon name, and target (optional) is the version
-        // { name: 'some-addon-name', target: '0.0.0' }
-      ]
-    }).then(() => {
-      return this.addPackagesToProject([
-        { name: 'oidc-client', target: '1.5.2' }
-      ]);
-    });
+    return this.addPackagesToProject([
+      { name: 'oidc-client', target: '1.5.2' }
+    ]);
   }
 };
