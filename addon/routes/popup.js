1a00d9282c77a6a7fb29a4889e637d77f1941ee4
Alex Barriere <superrugdr@gmail.com>
2019-08-05 15:12:58 -0400

    Added redirect flow to the app while maintaining compatibility with the popup version (#16)

--- addon/routes/popup.js
+++ addon/routes/popup.js
@@ -4,9 +4,17 @@ import { inject as service } from '@ember/service';
 export default Route.extend({
   session: service(),
 
-  init() {
+  async init() {
     this._super(...arguments);
-
-    this.get('session.userManager').signinPopupCallback();
+    let session = this.get('session');
+    if(session.usePopup)
+    {
+      await this.get('session.userManager').signinPopupCallback();
+    } 
+    else {
+      await this.get('session.userManager').signinRedirectCallback();
+      if(session.transitionToRedirect)
+      await session.authenticate(this.transitionTo(session.transitionToRedirect));
+    }
   }
 });
